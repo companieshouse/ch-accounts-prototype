229bd1c7422a07843a0997a04d841a9d6792bf23
jfrancis@companieshouse.gov.uk <jfrancis@companieshouse.gov.uk>
2016-07-05 15:42:36 +0100

    Added two journeys
    1 - CATO style
    2 - GDS, one thing per page style

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Service name goes here",
+  serviceName: "File your accounts",
 
   // Default port that prototype runs on
   port: '3000',
@@ -20,4 +20,3 @@ module.exports = {
   cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>'
 
 };
-	
