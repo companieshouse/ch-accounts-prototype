69730a7552d89e23edee2865206868fcb53b596c
jfranciswebdesign <jfrancis@companieshouse.gov.uk>
2016-09-19 10:43:24 +0100

    Revised start of journey

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -3,6 +3,19 @@ $(document).ready(function() {
     $('#choose-accounts-button').attr('disabled', 'true'); // Disables Next button on page load
     $('input:radio[name=choosetypeaccounts]').click(function() {
         var checkval = $(this).val();
-        $('#choose-accounts-button').prop('disabled', !(checkval == 'abridged' || checkval == 'micro' || checkval == 'dormant'));
+      //  $('#choose-accounts-button').prop('disabled', !(checkval == 'abridged' || checkval == 'micro' || checkval == 'dormant'));
+        $('#choose-accounts-button').prop('disabled', !(checkval == 'abridged'));
+
     });
+});
+
+//Start page - disabled next button, until user confirms they have a set of prepared accounts ready
+$(document).ready(function(){
+  $('#accounts-start-button').attr('disabled', 'true'); // Disables Next button on page load
+  $('input:checkbox[name=prepared]').click(function() {
+      var checkval = $(this).val();
+    //  $('#choose-accounts-button').prop('disabled', !(checkval == 'abridged' || checkval == 'micro' || checkval == 'dormant'));
+      $('#accounts-start-button').prop('disabled', !(checkval == 'prepared-yes'));
+
+  });
 });
