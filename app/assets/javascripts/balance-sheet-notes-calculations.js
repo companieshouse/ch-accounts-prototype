1d2b522c4fad766d63251e07d90eb9b4f30d5bc6
jfranciswebdesign <jfrancis@companieshouse.gov.uk>
2016-10-13 08:14:44 +0100

    Correcting fields (amortisation on Tangible was wrong)

--- app/assets/javascripts/balance-sheet-notes-calculations.js
+++ app/assets/javascripts/balance-sheet-notes-calculations.js
@@ -11,17 +11,17 @@ $(document).ready(function() {
      $('#tangible-cost-at-start, #tangible-cost-additions, #tangible-cost-disposals, #tangible-cost-transfers').change(compute);
 });
 
-// Tangible assets note: Amortisation AT END
+// Tangible assets note: depreciation AT END
 $(document).ready(function() {
   function compute() {
-       var tangibleamortisationatstart = $('#tangible-amortisation-at-start').val();
-       var tangibleamortisationchargeforyear = $('#tangible-amortisation-charge-for-year').val();
-       var tangibleamortisationondisposals = $('#tangible-amortisation-on-disposals').val();
-       var tangibleamortisationotheradjustments = $('#tangible-amortisation-other-adjustments').val();
-       var total = +tangibleamortisationatstart + +tangibleamortisationchargeforyear + +tangibleamortisationondisposals + +tangibleamortisationotheradjustments;
-       $('#tangible-amortisation-at-end').val(total);
+       var tangibledepreciationatstart = $('#tangible-depreciation-at-start').val();
+       var tangibledepreciationchargeforyear = $('#tangible-depreciation-charge-for-year').val();
+       var tangibledepreciationondisposals = $('#tangible-depreciation-on-disposals').val();
+       var tangibledepreciationotheradjustments = $('#tangible-depreciation-other-adjustments').val();
+       var total = +tangibledepreciationatstart + +tangibledepreciationchargeforyear + +tangibledepreciationondisposals + +tangibledepreciationotheradjustments;
+       $('#tangible-depreciation-at-end').val(total);
      }
-     $('#tangible-amortisation-at-start, #tangible-amortisation-charge-for-year, #tangible-amortisation-on-disposals, #tangible-amortisation-other-adjustments').change(compute);
+     $('#tangible-depreciation-at-start, #tangible-depreciation-charge-for-year, #tangible-depreciation-on-disposals, #tangible-depreciation-other-adjustments').change(compute);
 });
 
 // Tangible assets note: net book value end
