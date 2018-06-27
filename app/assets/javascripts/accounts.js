eeb85511cd92cdd48e2f0816ac15971e3065a53b
jfranciswebdesign <jfrancis@companieshouse.gov.uk>
2018-06-27 09:32:11 +0100

    Typo

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -1,13 +1,18 @@
 //Choose type of accounts - disabled next button, only undisable it if user selects Abridged, Micro or Dormant
-$(document).ready(function() {
-    $('#choose-accounts-button').attr('disabled', 'true'); // Disables Next button on page load
-    $('input:radio[name=choosetypeaccounts]').click(function() {
-        var checkval = $(this).val();
-      //  $('#choose-accounts-button').prop('disabled', !(checkval == 'abridged' || checkval == 'micro' || checkval == 'dormant'));
-        $('#choose-accounts-button').prop('disabled', !(checkval == 'abridged'));
+$( document )
+	.ready( function () {
+		$( '#choose-accounts-button' )
+			.attr( 'disabled', 'true' ); // Disables Next button on page load
+		$( 'input:radio[name=choosetypeaccounts]' )
+			.click( function () {
+				var checkval = $( this )
+					.val();
+				//  $('#choose-accounts-button').prop('disabled', !(checkval == 'abridged' || checkval == 'micro' || checkval == 'dormant'));
+				$( '#choose-accounts-button' )
+					.prop( 'disabled', !( checkval == 'abridged' ) );
 
-    });
-});
+			} );
+	} );
 
 //Start page - disabled next button, until user confirms they have a set of prepared accounts ready
 //$(document).ready(function(){
@@ -33,50 +38,53 @@ $(document).ready(function() {
 
 
 //// Copied from DEV version, reveals help as soon as user interacts with numeric inputs
-$(function() {
+$( function () {
 
 	// Fixed assets
-	attachOnChangeListener([$('#intangible-assets-current'),  $('#tangible-assets-current'),  $('#investments-fixed-assets-current')], 'total-fixed-assets-current')
-	attachOnChangeListener([$('#intangible-assets-previous'),  $('#tangible-assets-previous'),  $('#investments-fixed-assets-previous')], 'total-fixed-assets-previous' )
+	attachOnChangeListener( [ $( '#intangible-assets-current' ), $( '#tangible-assets-current' ), $( '#investments-fixed-assets-current' ) ], 'total-fixed-assets-current' )
+	attachOnChangeListener( [ $( '#intangible-assets-previous' ), $( '#tangible-assets-previous' ), $( '#investments-fixed-assets-previous' ) ], 'total-fixed-assets-previous' )
 
 	// Current assets
-	attachOnChangeListener([$('#stocks-current'),  $('#debtors-current'),  $('#cash-at-bank-and-in-hand-current'), $('#investments-current-assets-current')], 'total-current-assets-current' )
-	attachOnChangeListener([$('#stocks-previous'),  $('#debtors-previous'),  $('#cash-at-bank-and-in-hand-previous'), $('#investments-current-assets-previous')], 'total-current-assets-previous' )
+	attachOnChangeListener( [ $( '#stocks-current' ), $( '#debtors-current' ), $( '#cash-at-bank-and-in-hand-current' ), $( '#investments-current-assets-current' ) ], 'total-current-assets-current' )
+	attachOnChangeListener( [ $( '#stocks-previous' ), $( '#debtors-previous' ), $( '#cash-at-bank-and-in-hand-previous' ), $( '#investments-current-assets-previous' ) ], 'total-current-assets-previous' )
 
 	// Net current assets
-	attachOnChangeListener([$('#total-current-assets-current'),  $('#creditors-amounts-falling-due-within-one-year-current')], 'net-current-assets-liabilities-current', true )
-	attachOnChangeListener([$('#total-current-assets-previous'),  $('#creditors-amounts-falling-due-within-one-year-previous')], 'net-current-assets-liabilities-previous', true )
+	attachOnChangeListener( [ $( '#total-current-assets-current' ), $( '#creditors-amounts-falling-due-within-one-year-current' ) ], 'net-current-assets-liabilities-current', true )
+	attachOnChangeListener( [ $( '#total-current-assets-previous' ), $( '#creditors-amounts-falling-due-within-one-year-previous' ) ], 'net-current-assets-liabilities-previous', true )
 
 	// Total assets less current liabilities
-	attachOnChangeListener([$('#total-fixed-assets-current'),  $('#net-current-assets-liabilities-current')], 'total-assets-less-current-liabilities-current' )
-	attachOnChangeListener([$('#total-fixed-assets-previous'),  $('#net-current-assets-liabilities-previous')], 'total-assets-less-current-liabilities-previous' )
+	attachOnChangeListener( [ $( '#total-fixed-assets-current' ), $( '#net-current-assets-liabilities-current' ) ], 'total-assets-less-current-liabilities-current' )
+	attachOnChangeListener( [ $( '#total-fixed-assets-previous' ), $( '#net-current-assets-liabilities-previous' ) ], 'total-assets-less-current-liabilities-previous' )
 
 	// Total net assets (liabilities)
-	attachOnChangeListener([$('#total-assets-less-current-liabilities-current'),  $('#creditors-amounts-falling-due-after-more-than-one-year-current'), $('#provision-for-liabilities-current')], 'total-net-assets-liabilities-current', true )
-	attachOnChangeListener([$('#total-assets-less-current-liabilities-previous'),  $('#creditors-amounts-falling-due-after-more-than-one-year-previous'), $('#provision-for-liabilities-previous')], 'total-net-assets-liabilities-previous', true )
+	attachOnChangeListener( [ $( '#total-assets-less-current-liabilities-current' ), $( '#creditors-amounts-falling-due-after-more-than-one-year-current' ), $( '#provision-for-liabilities-current' ) ], 'total-net-assets-liabilities-current', true )
+	attachOnChangeListener( [ $( '#total-assets-less-current-liabilities-previous' ), $( '#creditors-amounts-falling-due-after-more-than-one-year-previous' ), $( '#provision-for-liabilities-previous' ) ], 'total-net-assets-liabilities-previous', true )
 
-    //Capital and reserves
-	attachOnChangeListener([$('#called-up-share-capital-current'),  $('#share-premium-account-current'),  $('#revaluation-reserve-current'), $('#other-reserves-current'), $('#profit-and-loss-account-current')], 'total-shareholders-funds-current' )
-	attachOnChangeListener([$('#called-up-share-capital-previous'),  $('#share-premium-account-previous'),  $('#revaluation-reserve-previous'), $('#other-reserves-previous'), $('#profit-and-loss-account-previous')], 'total-shareholders-funds-previous' )
+	//Capital and reserves
+	attachOnChangeListener( [ $( '#called-up-share-capital-current' ), $( '#share-premium-account-current' ), $( '#revaluation-reserve-current' ), $( '#other-reserves-current' ), $( '#profit-and-loss-account-current' ) ], 'total-shareholders-funds-current' )
+	attachOnChangeListener( [ $( '#called-up-share-capital-previous' ), $( '#share-premium-account-previous' ), $( '#revaluation-reserve-previous' ), $( '#other-reserves-previous' ), $( '#profit-and-loss-account-previous' ) ], 'total-shareholders-funds-previous' )
 
-	$(".grid-row").focusin(function() {
-		var $this = $(this);
-		$('details').removeAttr('open');
-		$this.find('details').attr('open', 'true')
-		})
-});
+	$( ".grid-row" )
+		.focusin( function () {
+			var $this = $( this );
+			$( 'details' )
+				.removeAttr( 'open' );
+			$this.find( 'details' )
+				.attr( 'open', 'true' )
+		} )
+} );
 
 
-function attachOnChangeListener(arr, total, substract){
-	for(var i=0;i<arr.length;i++){
-		arr[i].on('change', function() {
-			if( substract ){
-				findDifference(arr, total)
+function attachOnChangeListener( arr, total, subtract ) {
+	for ( var i = 0; i < arr.length; i++ ) {
+		arr[ i ].on( 'change', function () {
+			if ( subtract ) {
+				findDifference( arr, total )
 			} else {
-				findTotal(arr, total)
+				findTotal( arr, total )
 			}
 
-		});
+		} );
 	}
 }
 
@@ -84,13 +92,15 @@ function attachOnChangeListener(arr, total, substract){
 
 
 
-// Show message when user clicks Delete something, eg Creditors note 
-$(function() {
-    $('#content-delete').click(function() {
-        $('#content-delete-warning').show();
-        return false;
-    });
-});
+// Show message when user clicks Delete something, eg Creditors note
+$( function () {
+	$( '#content-delete' )
+		.click( function () {
+			$( '#content-delete-warning' )
+				.show();
+			return false;
+		} );
+} );
 
 
 
@@ -108,23 +118,26 @@ $(function() {
 // });
 
 //About your business (2 from 3 for Micro eligibility)
-$('.micro-eligibility-questions').click(function(){
-  if($(this).find('input[type="radio"]:checked').length > 3)
-    {
-       $(".button").removeClass("disabled");
-    }
-  else
-    {
-      $(".button").addClass("disabled");
-    }
-});
+$( '.micro-eligibility-questions' )
+	.click( function () {
+		if ( $( this )
+			.find( 'input[type="radio"]:checked' )
+			.length > 3 ) {
+			$( ".button" )
+				.removeClass( "disabled" );
+		} else {
+			$( ".button" )
+				.addClass( "disabled" );
+		}
+	} );
 
-$('.micro-eligibility').click(function() {
-  if($('input[type=radio][value="Yes"]:checked').length > 1) {
-    window.location.replace("/accounts/accounts-choose-micros");
-  }
- else {
-   window.location.replace("/accounts/accounts-choose-full-or-abridged");
- }
- return false;
-});
+$( '.micro-eligibility' )
+	.click( function () {
+		if ( $( 'input[type=radio][value="Yes"]:checked' )
+			.length > 1 ) {
+			window.location.replace( "/accounts/accounts-choose-micros" );
+		} else {
+			window.location.replace( "/accounts/accounts-choose-full-or-abridged" );
+		}
+		return false;
+	} );
