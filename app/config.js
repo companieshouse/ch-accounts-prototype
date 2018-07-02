37249c47feefcfd845a4a4823cc6584a7e321391
jfranciswebdesign <jfrancis@companieshouse.gov.uk>
2018-07-02 14:18:44 +0100

    Updating to latest styles

--- app/config.js
+++ app/config.js
@@ -3,28 +3,28 @@
 // Note: prototype config can be overridden using environment variables (eg on heroku)
 
 module.exports = {
-  // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+	// Service name used in header. Eg: 'Renew your passport'
+	serviceName: 'File your accounts',
 
-  // Default port that prototype runs on
-  port: '3000',
+	// Default port that prototype runs on
+	port: '3000',
 
-  // Enable or disable password protection on production
-  useAuth: 'true',
+	// Enable or disable password protection on production
+	useAuth: 'true',
 
-  // Automatically stores form data, and send to all views
-  useAutoStoreData: 'true',
+	// Automatically stores form data, and send to all views
+	useAutoStoreData: 'true',
 
-  // Enable or disable built-in docs and examples.
-  useDocumentation: 'true',
+	// Enable or disable built-in docs and examples.
+	useDocumentation: 'true',
 
-  // Force HTTP to redirect to HTTPS on production
-  useHttps: 'true',
+	// Force HTTP to redirect to HTTPS on production
+	useHttps: 'true',
 
-  // Cookie warning - update link to service's cookie page.
-  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>',
+	// Cookie warning - update link to service's cookie page.
+	cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>',
 
-  // Enable or disable Browser Sync
-  useBrowserSync: 'true'
+	// Enable or disable Browser Sync
+	useBrowserSync: 'true'
 
 }
