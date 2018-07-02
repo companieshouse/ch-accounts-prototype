0e0d4bb9c8cf6aa5bb693bb93a969a1be167a098
jfranciswebdesign <jfrancis@companieshouse.gov.uk>
2018-07-02 11:30:01 +0100

    Updates to V7 of Prototype kit

--- app/config.js
+++ app/config.js
@@ -3,9 +3,8 @@
 // Note: prototype config can be overridden using environment variables (eg on heroku)
 
 module.exports = {
-
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "File company accounts",
+  serviceName: 'Service name goes here',
 
   // Default port that prototype runs on
   port: '3000',
@@ -13,10 +12,19 @@ module.exports = {
   // Enable or disable password protection on production
   useAuth: 'true',
 
-  // Force HTTP to redirect to HTTPs on production
+  // Automatically stores form data, and send to all views
+  useAutoStoreData: 'true',
+
+  // Enable or disable built-in docs and examples.
+  useDocumentation: 'true',
+
+  // Force HTTP to redirect to HTTPS on production
   useHttps: 'true',
 
   // Cookie warning - update link to service's cookie page.
-  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>'
+  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>',
 
-};
+  // Enable or disable Browser Sync
+  useBrowserSync: 'true'
+
+}
