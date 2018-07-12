c99a3279e96623aa6c2206069025edbe7a2d8520
jfranciswebdesign <jfrancis@companieshouse.gov.uk>
2018-07-12 11:12:19 +0100

    Updating CHS search pages styling

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,20 @@
 var express = require( 'express' );
 var router = express.Router();
 
+
 // CICS
 require( './routes/routes-cics.js' )( router )
 // Older - may be deleted at some point
 require( './routes/routes-older.js' )( router )
+
+// Start
+router.get( '/', function ( req, res ) {
+	req.session.destroy()
+	res.render( 'index' )
+} )
+router.get( 'index', function ( req, res ) {
+	req.session.destroy()
+	res.render( 'index' )
+} )
 
 module.exports = router
