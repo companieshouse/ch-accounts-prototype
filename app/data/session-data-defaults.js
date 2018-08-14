9cb2fa3abfa48208ebf2dd0c6be0e289a41bb59f
jfranciswebdesign <jfrancis@companieshouse.gov.uk>
2018-08-14 16:16:22 +0100

    Updated CIC info

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -35,7 +35,7 @@ module.exports = {
 	"cics-activities": "",
 	"cics-consultation-stakeholders": "There has not been a consultation with stakeholders",
 	"cicsDirectorsRemuneration": "No remuneration was received",
-	"cics-transfer-assets": "No transfer of assets other than for full consideration has been made",
+	"cics-transfer-assets": "No transfer of assets that have not reached market value",
 
 	//Small Full Balance sheet
 	"called-up-share-capital-not-paid-current": "",
